0075945fefaa74a36e065193d7c3869aa58b4dba
AvaterClasher <avater.clasher@gmail.com>
2024-02-17 14:49:34 +0530

    feat: :sparkles: added a post function for the api request

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #[macro_use]
 extern crate rocket;
 
+use dotenv_codegen::dotenv;
 use reqwest::blocking::Client;
-use rocket::serde::json::{json, Json};
+use rocket::http::Status;
+use rocket::serde::json::{json, serde_json, Json};
 use rocket::serde::{Deserialize, Serialize};
 
 // Json Structure for User Message
@@ -19,13 +21,41 @@ struct ChatResponse {
     chatbot_response: String,
 }
 
-#[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+// Added a post function for the api request
+#[post("/chat", format = "json", data = "<input>")]
+fn chat(input: Json<ChatRequest>) -> Result<Json<ChatResponse>, Status> {
+    let api_key = dotenv!("OPEN_AI_API");
+    let endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions";
+    let client = Client::new();
+    let prompt = "How are you ?";
+
+    let response = client
+        .post(endpoint)
+        .header("Authorization", format!("Bearer {}", api_key))
+        .json(&json!({
+            "prompt": prompt,
+            "max_tokens": 150,
+        }))
+        .send();
+
+    match response {
+        Ok(res) => {
+            if let Ok(json) = res.json::<serde_json::Value>() {
+                if let Some(completions) = json.get("choices") {
+                    if let Some(completion) = completions[0].get("text") {
+                        let chatbot_response = completion.as_str().unwrap().to_string();
+                        return Ok(Json(ChatResponse { chatbot_response }));
+                    }
+                }
+            }
+        }
+        Err(_) => return Err(Status::InternalServerError),
+    }
+    Err(Status::InternalServerError)
 }
 
 // Rocket Main function
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build().mount("/", routes![chat])
 }
