1eb72f9da33ccbd4bf334f9486aa0ff2c8e74205
AvaterClasher <avater.clasher@gmail.com>
2024-02-17 14:33:30 +0530

    feat: :sparkles: added json structure for Chatrequest and Chatresponse

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 #[macro_use]
 extern crate rocket;
 
+use reqwest::blocking::Client;
+use rocket::serde::json::{json, Json};
+use rocket::serde::{Deserialize, Serialize};
+
+// Json Structure for User Message
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct ChatRequest {
+    user_message: String,
+}
+
+// Json Structure for Chatbot Response
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct ChatResponse {
+    chatbot_response: String,
+}
+
 #[get("/")]
 fn hello() -> &'static str {
     "Hello, world!"
